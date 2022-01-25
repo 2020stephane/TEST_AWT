89a7cd696372a17802d3fa7dd568ceea7dea5f75
stephane <stephanebrisse@gmail.com>
2022-01-25 16:38:24 +0100

    test avec java.awt

--- src/Main.java
+++ src/Main.java
@@ -5,35 +5,40 @@ import java.awt.event.*;
 
 public class Main extends Frame implements WindowListener {
 
-    Frame fenetre;
+    Frame frame;
     Label label;
     Button bouton;
     MenuBar menuBar;
     Menu mfile;
     MenuItem mnew;
     MenuItem mopen;
+    Panel mypanel;
 
     public Main() {
 
         super();
-        fenetre = new Frame("titre fenetre");
-
-        fenetre.setBounds(600,200,400,250);
+        frame = new Frame("titre fenetre");
+        frame.setBounds(600,200,400,250);
         label = new Label("Affichez ici le texte du label");
         bouton = new Button("appuyer ici");
-        fenetre.setLayout(new BorderLayout());
-        fenetre.add(label, BorderLayout.NORTH);
-        fenetre.add(bouton, BorderLayout.SOUTH);
+        //fenetre.setLayout(new BorderLayout());
+        //fenetre.add(label, BorderLayout.NORTH);
+        //fenetre.add(bouton, BorderLayout.SOUTH);
         menuBar = new MenuBar();
         mfile = new Menu("File");
         menuBar.add(mfile);
-        fenetre.setMenuBar(menuBar);
+        frame.setMenuBar(menuBar);
         mnew = new MenuItem("New");
         mopen = new MenuItem("Open");
         mfile.add(mnew);
         mfile.add(mopen);
-        fenetre.addWindowListener(this);
-        fenetre.setVisible(true);
+        mypanel = new Panel();
+        mypanel.setLayout(new BorderLayout());
+        mypanel.add(label, BorderLayout.NORTH);
+        mypanel.add(bouton, BorderLayout.SOUTH);
+        frame.add(mypanel);
+        frame.addWindowListener(this);
+        frame.setVisible(true);
     }
 
 
@@ -46,7 +51,7 @@ public class Main extends Frame implements WindowListener {
     public void windowOpened(WindowEvent e) {   }
 
     @Override
-    public void windowClosing(WindowEvent e) { fenetre.dispose();  }
+    public void windowClosing(WindowEvent e) { frame.dispose();  }
 
     @Override
     public void windowClosed(WindowEvent e) {  }
